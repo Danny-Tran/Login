b0a3113c8656a76d1ce9eab472a5b3bd29b55d7e
Danny-Tran <truong_tran@live.com>
2020-09-30 13:39:01 -0600

    working on nav bar and login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import "./App.css";
+import Routes from './Routes';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          
+          <Navbar.Brand>
+            <Link to="/">Nav bar Brand</Link>
+          </Navbar.Brand>
+
+          <Button className="pull-right" bsStyle="primary">
+            <Link to="/login" style={{ color: 'white', textDecoration: 'none' }}>Login</Link>
+          </Button>
+
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+          <div className="lander">
+            <h1>HOME</h1>
+            <p>Please login to access your account</p>
+          </div>
+        </div>
+      );
+};
+
+export default Home;
