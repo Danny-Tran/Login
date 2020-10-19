40bc43597a4c1b01dac5c74a51be419b8edad9ce
Danny-Tran <truong_tran@live.com>
2020-10-19 11:36:33 -0600

    add 404 to route

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from './containers/Login'
+import NotFound from './containers/NotFound'
 
 // switch that handle redirection from gome to login
 export default function Routes() {
@@ -15,7 +16,12 @@ export default function Routes() {
       <Route exact path="/logins">
         <Login />
       </Route>
-      
+
+      {/* Finally, catch all unmatched routes */}
+      <Route>
+        <NotFound />
+      </Route>
+
     </Switch>
   );
 }
